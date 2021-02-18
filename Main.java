03a05eaca633f24850c335e941b607bd6c6ff0d6
kris0892 <kostk0892@wrdsb.ca>
2021-02-18 16:39:55 +0000

    let the user know their age

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  *The hello world program 
  * @author Kristina Kostadinovska 
@@ -9,7 +11,30 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    // Ask the user for thier name 
+    System.out.println("Please enter your name:");
+
+    // creating a Scanner for user input 
+    Scanner input = new Scanner(System.in);
+
+    // Declare and initialize the name variable
+    String name = input.nextLine();
+
+    // say hello to the user 
+    System.out.println("Hello " + name);
+
+    // ask the user when they were born
+    System.out.println("Please enter the year you were born:");
+
+    // create the variable to store the birth year 
+    int birthYear = input.nextInt(); 
+
+    // calculate the approximate age 
+    final int CURRENT_YEAR = 2021; 
     
+    int age = CURRENT_YEAR - birthYear; 
+
+    // let the user know their age 
+    System.out.println("You are approximately " + age + " years old."); 
   }
 }
